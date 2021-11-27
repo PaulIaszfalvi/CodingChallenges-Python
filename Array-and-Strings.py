7f0001cb0b430862c772439324e780c8aca30a0e
Paul <paul.iaszfalvi@gmail.com>
2021-11-26 22:57:51 -0500

    Started working on Chapter 1

--- Array-and-Strings.py
+++ Array-and-Strings.py
@@ -1,1 +1,8 @@
-print("hello")
+
+#Check to see if the string provided contains all unique elements
+def isUnique(s):
+    lengthOfString = len(s)
+    stringSet = set(char for char in s)   #create a set of items using the elements of the string
+    return len(stringSet) == lengthOfString     #if length of string doesn't match length of set, ther are duplicates
+
+print(isUnique("Hello"))

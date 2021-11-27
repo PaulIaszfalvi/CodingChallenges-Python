58bed086c23e50033f562f58e1a43ca0b2d1f936
Paul <paul.iaszfalvi@gmail.com>
2021-11-26 23:15:54 -0500

    Chapter 1

--- Array-and-Strings.py
+++ Array-and-Strings.py
@@ -2,7 +2,22 @@
 #Check to see if the string provided contains all unique elements
 def isUnique(s):
     lengthOfString = len(s)
-    stringSet = set(char for char in s)   #create a set of items using the elements of the string
+    stringSet = set(char for char in s)     #create a set of items using the elements of the string
     return len(stringSet) == lengthOfString     #if length of string doesn't match length of set, ther are duplicates
 
+#Checks for permutation
+def isPermutation(s):
+    stringReverse = s[::-1]     #Flips the string
+    return s == stringReverse       #Compares the original to the reversed
+
+#Convert spaces to %20
+def URLify(s, l):
+    newString = s[:l:]      #Cut off white spaces after length given
+    return newString.replace(" ", "%20")    #Replace spaces with %20
+
 print(isUnique("Hello"))
+print(isPermutation("Hello"))
+print(isPermutation("racecar"))
+
+myString = "Hello World How Are You?      "
+print(URLify(myString, 24))

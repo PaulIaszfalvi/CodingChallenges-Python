f630b7cbaeaf1877802349919f1fca664af75da0
Paul <paul.iaszfalvi@gmail.com>
2023-09-02 20:55:17 -0400

    Sep 2 2023 Challenges

---
+++ plus_one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits):
+
+        digit = ""
+
+        for s in digits:
+            digit += str(s)
+
+        digit = int(digit) + 1
+        digits = [int(i) for i in str(digit)]
+
+        return digits
+
+
+digits = [9]
+print(Solution().plusOne(digits))
+digits = [1,2,3,4]
+print(Solution().plusOne(digits))

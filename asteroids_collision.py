12516db9dad46c10480ab569fe62e36fb8dfa30a
Paul <paul.iaszfalvi@gmail.com>
2023-09-16 11:51:51 -0400

    update

--- asteroids_collision.py
+++ asteroids_collision.py
@@ -1,17 +1,11 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         
-        j = 0
-        n = len(asteroids)
-
-        for i in range(n):
-            asteroid = asteroids[i]
-            while j > 0 and asteroids[j - 1] > 0 and asteroid < 0 and asteroids[j - 1] < abs(asteroid):
-                j -= 1
-
-            if j == 0 or asteroid > 0 or asteroids[j - 1] < 0:
-                asteroids[j] = asteroid
-                j += 1
-            elif asteroids[j - 1] == abs(asteroid):
-                j -= 1
-        return asteroids[:j]
+        s = []
+        for a in asteroids:
+            while s and s[-1] > 0 and a < 0:
+                if s[-1] + a < 0: s.pop()
+                elif s[-1] + a > 0: break
+                else: s.pop(); break
+            else: s.append(a)
+        return s

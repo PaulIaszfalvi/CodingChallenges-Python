4ca0d90f959669563d84f7cd0ff8106520331312
paul <paul.iaszfalvi@gmail.com>
2023-12-14 19:37:59 -0500

    challenge

---
+++ destin_city.py
@@ -0,0 +1,28 @@
+class Solution:
+    def destCity(self, p: List[List[str]]) -> str:
+
+        # Sets (easiest)
+
+        start = set()
+        end = set()
+
+        for l in p:
+            start.add(l[0])
+            end.add(l[1])
+        return list(end - start)[0]
+              
+        # Solution 2
+        p = sorted(p, key=lambda x: x[1])
+              
+        return p[-1][1]
+        
+        # Solution 1
+        # start = p[0][0]
+        # end = [p[0][1]]
+      
+        # for i in p:
+        #     if i[0] in end or i[1] in end:
+        #         end.append(i[1])
+                
+        # print(end)
+        # return end[-1]

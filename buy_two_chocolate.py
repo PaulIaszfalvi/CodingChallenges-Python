95692312fc1ea67c032b6160ff7c88ab20b981fc
paul <paul.iaszfalvi@gmail.com>
2023-12-19 19:15:42 -0500

    daily

---
+++ buy_two_chocolate.py
@@ -0,0 +1,23 @@
+class Solution:
+    def buyChoco(self, prices: List[int], money: int) -> int:
+
+        # Solution 2
+
+        s1, s2 = 100, 100
+
+        for x in prices:
+            if x < s1:
+               s2 = s1
+               s1 = x
+            elif x < s2:
+                s2 = x
+
+        result = money - s1 - s2
+
+        return result if result >= 0 else money
+
+        # Solution 1
+        
+        # prices.sort()
+        # result = money - sum(prices[:2])
+        # return result if result >= 0 else money

540ff6883f672836b587753100d67e0b36ad7d1e
Paul <Paul.Iaszfalvi@gmail.com>
2024-01-18 09:29:16 -0500

    update to daily

--- climbing_stairs_2.py
+++ climbing_stairs_2.py
@@ -9,3 +9,27 @@ class Solution:
             f, s = s, s + f
         
         return s
+    
+        # Using matrix multiplication to get the sequence
+           
+        # if n <= 1:
+        #     return 1
+
+        # def multiply(a, b):
+        #     return [
+        #         [a[0][0] * b[0][0] + a[0][1] * b[1][0], a[0][0] * b[0][1] + a[0][1] * b[1][1]],
+        #         [a[1][0] * b[0][0] + a[1][1] * b[1][0], a[1][0] * b[0][1] + a[1][1] * b[1][1]]
+        #     ]
+
+        # def power(matrix, exp):
+        #     result = [[1, 0], [0, 1]]
+        #     while exp > 0:
+        #         if exp % 2 == 1:
+        #             result = multiply(result, matrix)
+        #         matrix = multiply(matrix, matrix)
+        #         exp //= 2
+        #     return result
+
+        # base_matrix = [[1, 1], [1, 0]]
+        # result_matrix = power(base_matrix, n - 1)
+        # return result_matrix[0][0] + result_matrix[0][1]

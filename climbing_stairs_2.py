063a5d66115b0b67652eaea51fa8c38ae6e0f507
Paul <Paul.Iaszfalvi@gmail.com>
2024-01-18 09:31:02 -0500

    using recursion

--- climbing_stairs_2.py
+++ climbing_stairs_2.py
@@ -33,3 +33,28 @@ class Solution:
         # base_matrix = [[1, 1], [1, 0]]
         # result_matrix = power(base_matrix, n - 1)
         # return result_matrix[0][0] + result_matrix[0][1]
+    
+        # Same as above but recursive
+    
+        if n <= 1:
+            return 1
+
+        def multiply(a, b):
+            return [
+                [a[0][0] * b[0][0] + a[0][1] * b[1][0], a[0][0] * b[0][1] + a[0][1] * b[1][1]],
+                [a[1][0] * b[0][0] + a[1][1] * b[1][0], a[1][0] * b[0][1] + a[1][1] * b[1][1]]
+            ]
+
+        def power(matrix, exp):
+            if exp == 0:
+                return [[1, 0], [0, 1]]
+            if exp % 2 == 0:
+                half_pow = power(matrix, exp // 2)
+                return multiply(half_pow, half_pow)
+            else:
+                return multiply(matrix, power(matrix, exp - 1))
+
+        base_matrix = [[1, 1], [1, 0]]
+        result_matrix = power(base_matrix, n - 1)
+        return sum(result_matrix[0])
+

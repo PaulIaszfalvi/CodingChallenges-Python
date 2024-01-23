f7312236978a6242c3e8df08bae22e2696fac7cc
Paul <Paul.Iaszfalvi@gmail.com>
2024-01-23 11:40:02 -0500

    commented some code (bad vars)

--- find_error_nums.py
+++ find_error_nums.py
@@ -1,8 +1,10 @@
 class Solution:
     def findErrorNums(self, nums):
        
+       # sum of all numbers
        l = len(nums) * (len(nums)+ 1) // 2
-
+       # sum of unique numbers 
        s = sum(set(nums))
    
+       # return duplicate and misisng number 
        return [sum(nums)-s,l-s]

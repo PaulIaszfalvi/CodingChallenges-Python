a1a4cb90b393bb7ccc980f6043f4859a13a01863
Paul <Paul.Iaszfalvi@gmail.com>
2024-04-02 13:00:08 -0400

    daily

--- length_of_last_words.py
+++ length_of_last_words.py
@@ -1,4 +1,14 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+
+        # Solution 2
+    
+        s = s.strip()  # Trim leading and trailing spaces
+        if not s:  # If the string is empty after trimming, return 0
+            return 0
+        return len(s.split()[-1])  # Return the length of the last word
+
+
+        # Solution 1
               
         return len(s.split()[-1])

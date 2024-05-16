02baa3dfee9f0545501cb14575b8baedadf839b8
Paul <Paul.Iaszfalvi@gmail.com>
2024-05-16 11:10:24 -0400

    daily

---
+++ evaluateTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+
+        # Solution 1
+
+        def rec(root):
+            if not root:
+                return None
+
+            if root.val == 0:
+                return False
+            elif root.val == 1:
+                return True
+            elif root.val == 2:
+                return rec(root.left) or rec(root.right)
+            elif root.val == 3:
+                return rec(root.left) and rec(root.right)
+
+        return rec(root)

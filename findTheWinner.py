f08d1020181ce0e6c73e4fc7eec3079f4605c98f
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2024-07-08 09:46:40 -0400

    Update findTheWinner.py

--- findTheWinner.py
+++ findTheWinner.py
@@ -1,5 +1,18 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
+
+        # Solution 2
+        winner = 0  # Base case: for n = 1, the winner is index 0
+        
+        # Iterate through each number from 2 to n to find the position
+        for i in range(1, n + 1):
+            winner = (winner + k) % i
+
+        # Return the winner position adjusted to 1-based index
+        return winner + 1
+
+        # Solution 1
+        
         # Create a list of participants
         l = [x for x in range(1, n + 1)]
         pos = 0

aa4bcdddd69fd08725568f4b2904f74b509306f4
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2024-08-01 09:39:42 -0400

    Create countSeniors.py

---
+++ countSeniors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+
+        c = 0
+        
+        for x in details:        
+            if int(x[11:13]) > 60:
+                c += 1                
+
+        return c

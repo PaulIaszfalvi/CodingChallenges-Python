c99f1177bb359062c18e063faaa91f6954af45b9
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2024-12-02 08:44:49 -0500

    Update isPrefixOfWord.py

--- isPrefixOfWord.py
+++ isPrefixOfWord.py
@@ -1,6 +1,20 @@
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
 
+        # Solution 2
+
+        arr = list(sentence.split())
+        
+        for i, v in enumerate(arr):
+        
+            if searchWord == v[0 : len(searchWord)]:
+                return i+1
+            
+        return -1
+
+
+        # Solution 1
+
         arr = list(sentence.split())
         
         for i, v in enumerate(arr):

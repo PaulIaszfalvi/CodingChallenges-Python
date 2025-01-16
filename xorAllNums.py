df4167953640c643cb3325496c5ab95c015d2a4b
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2025-01-16 17:02:21 -0500

    Update xorAllNums.py

--- xorAllNums.py
+++ xorAllNums.py
@@ -1,6 +1,23 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
 
+        # Solution 4
+        
+        xor_result = 0
+
+        # If nums2 has an odd length, every element in nums1 affects the result
+        if len(nums2) % 2 == 1:
+            for num in nums1:
+                xor_result ^= num
+
+        # If nums1 has an odd length, every element in nums2 affects the result
+        if len(nums1) % 2 == 1:
+            for num in nums2:
+                xor_result ^= num
+
+        return xor_result
+
+
       # Solution 3
 
         xor1 = reduce(lambda x, y: x ^ y, nums1, 0)

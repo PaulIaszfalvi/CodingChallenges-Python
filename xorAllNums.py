27f807a38298997160ff7fc95b72c855cd73eb11
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2025-01-16 09:15:48 -0500

    Update xorAllNums.py

--- xorAllNums.py
+++ xorAllNums.py
@@ -1,12 +1,25 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
 
-      # Solution 2
+      # Solution 3
 
         xor1 = reduce(lambda x, y: x ^ y, nums1, 0)
         xor2 = reduce(lambda x, y: x ^ y, nums2, 0)
 
         return (xor1 if len(nums2) % 2 else 0) ^ (xor2 if len(nums1) % 2 else 0)
+
+        # Solution 2
+
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        nums3 = []
+        
+        for n in nums1:
+            for m in nums2:
+                nums3.append(n^m)
+
+        return reduce(lambda x, y: x ^ y, nums3)
+        
 
       # Solution 1
 

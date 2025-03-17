286c18d3843a328f4c06ea8f53f973fbad4f19db
Paul Iaszfalvi <75687229+PaulIaszfalvi@users.noreply.github.com>
2025-03-17 09:03:40 -0400

    Update divideArray.py

--- divideArray.py
+++ divideArray.py
@@ -1,5 +1,12 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
+
+        # Solution 2
+
+        d = Counter(nums)
+        return all(count % 2 == 0 for count in d.values())
+
+        # Solution 1
         
         l = len(nums)
         d = Counter(nums)
